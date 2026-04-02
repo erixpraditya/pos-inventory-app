6796765a2f05a3202623077a056965178d381de7
erixpraditya <erixpradityaa@gmail.com>
2026-04-02 12:35:53 +0700

    update
    mengubah timzone, mengganti route dan membuat template sederhana memakai tailwind

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Base of template</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+<body>
+<div class="text-center">
+    <h1 class="text-3xl font-bold text-indigo-600 text-underline mt-4">Base Of Template Nya</h1>
+    <p class="text-sm text-gray-500 mt-4">Ini adalah base template yang akan digunakan untuk semua halaman</p>
+    <button class="bg-indigo-600 text-white px-4 py-2 rounded-full mt-4">Button</button>
+</div>
+
+</body>
+</html>
